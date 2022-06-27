d6d66cbdea160ec5490ee4872886014d384ba263
James Ball <jamie.j.ball@gmail.com>
2022-06-27 15:51:51 -0700

    select all womens and mens stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,9 +1,37 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(
+  name: "Surry",
+  annual_revenue: 224_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+Store.create(
+  name: "Whistler",
+  annual_revenue: 1_900_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+Store.create(
+  name: "Yaletown",
+  annual_revenue: 430_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true, womens_apparel: false)
+@mens_stores.each { |store| p store.name }
+@womens_stores =
+  Store.where(
+    mens_apparel: false,
+    womens_apparel: true,
+    annual_revenue: ...1_000_000
+  )
+@womens_stores.each { |store| p store.name }
