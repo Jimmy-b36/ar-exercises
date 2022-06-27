a2b80c76bcf423eb6b27e9a2260623d13ddb1c57
James Ball <jamie.j.ball@gmail.com>
2022-06-27 16:56:54 -0700

    add validators

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,15 @@
 class Employee < ActiveRecord::Base
-  belongs_to :store
+  belongs_to :store, required: true
+  has_one :store
+  validates :first_name, :last_name, presence: true
+
+  validates :hourly_rate,
+            numericality: {
+              only_integer: true
+            },
+            comparison: {
+              greater_than: 40,
+              less_than: 200
+            },
+            presence: true
 end
