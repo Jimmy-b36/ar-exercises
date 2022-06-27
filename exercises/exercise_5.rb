889a56ab5440802380502116e478c01717baacba
James Ball <jamie.j.ball@gmail.com>
2022-06-27 16:12:42 -0700

    get average revenue & total revenue of all stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,13 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
 
 puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+p @total_revenue = Store.sum(:annual_revenue)
+@average_revenue = Store.average(:annual_revenue)
+p @average_revenue
