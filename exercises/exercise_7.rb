2cf86376fed02fdbd6a7e6998b9b9e107b90056f
James Ball <jamie.j.ball@gmail.com>
2022-06-27 16:56:46 -0700

    add input from user

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,12 +1,18 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
-require_relative './exercise_4'
-require_relative './exercise_5'
-require_relative './exercise_6'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
+require_relative "./exercise_4"
+require_relative "./exercise_5"
+require_relative "./exercise_6"
 
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+p "enter a store name"
+print "> "
+@user_input = gets.chomp
+@new_store = Store.create(name: @user_input)
+p @new_store.valid?
+p @new_store.errors.full_messages
