4bbf7e90ca7b17ba315bd63743d6dbf183a34d05
James Ball <jamie.j.ball@gmail.com>
2022-06-27 15:28:26 -0700

    delete store3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,11 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
 
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@stores3 = Store.find_by(id: 3)
+@stores3.destroy
+p Store.count()
